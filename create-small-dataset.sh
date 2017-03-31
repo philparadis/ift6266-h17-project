24cd5a0a5cf94d8f9104a9cfa329ae84a7ba4d89
Philippe Paradis <philippe.paradis@gmail.com>
2017-03-31 19:56:05 -0400

    Update create-small-dataset.sh script

--- create-small-dataset.sh
+++ create-small-dataset.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 SIZE=2000
-mkdir -p small_datasets/mscoco_inpainting/inpainting/train2014/
-cp datasets/mscoco_inpainting/inpainting/*.pkl small_datasets/mscoco_inpainting/inpainting
-ls -1 datasets/mscoco_inpainting/inpainting/train2014/ | head -n $SIZE | xargs -n1 -I{} cp datasets/mscoco_inpainting/inpainting/train2014/'{}' small_datasets/mscoco_inpainting/inpainting/train2014/
+
+SAVE_DIR=""
+
+if [[ -d "/Tmp" ]]; then
+    SAVE_DIR="/Tmp"
+elif [[ -d "/tmp" ]]; then
+    SAVE_DIR="/tmp"
+else
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+    SAVE_DIR="$SCRIPT_DIR"
+fi
+
+user_dir="${SAVE_DIR}/${USER}"
+target_dir="mscoco"
+
+pushd "$user_dir" &>/dev/null
+mkdir -p mscoco_small/train2014
+cp mscoco/*.pkl mscoco_small/
+ls -1 mscoco/train2014/ | head -n $SIZE | xargs -n1 -I{} cp mscoco/train2014/'{}' mscoco_small/train2014/
+popd &>/dev/null
+
+ln -sf "${user_dir}/mscoco_small" mscoco
