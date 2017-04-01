b9fe4b2525eae4ce83db69bec5b1d0da26161705
Philippe Paradis <philippe.paradis@gmail.com>
2017-03-31 20:01:23 -0400

    Simplify code for saving results

--- save_results.py
+++ save_results.py
@@ -1,82 +1,79 @@
 ### Utilities functions
 
 import os
+from os.path import join
 import sys
 import numpy as np
 import PIL.Image as Image
 from keras.utils import plot_model
 
 import settings
+from settings import BASE_DIR
+from settings import MODELS_DIR
+from settings import EPOCHS_DIR
+from settings import PERF_DIR
+from settings import SAMPLES_DIR
+from settings import PRED_DIR
+from settings import ASSETS_DIR
+from settings import HTML_DIR
 
 ## Your model will be saved in:                           models/<experiment_name>.h5
 ## A summary of your model architecture will saved be in: models/summary_<experiment_name>.txt
 ## Your model's performance will be saved in:             models/performance_<experiment_name>.txt
 
-def save_model_info(model):
-    out_dir = "models/"
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
+def save_keras_model_info(model):
+    settings.touch_dir(MODELS_DIR)
     ### Save model architecture and weights to .h5 file
-    model.save(os.path.join(out_dir, settings.EXP_NAME + '.h5')) 
-
+    model.save(join(MODELS_DIR, 'trained_model.h5')) 
     ### Write an image that represents the model's architecture
-    plot_model(model, to_file=os.path.join(out_dir, 'architecture_' + settings.EXP_NAME + '.png'), show_shapes=True)
-
+    plot_model(model, to_file=join(MODELS_DIR, 'plot_model.png'), show_shapes=True)
     ### Output a summary of the model, including the various layers, activations and total number of weights
     old_stdout = sys.stdout
-    sys.stdout = open(os.path.join(out_dir, 'summary_' + settings.EXP_NAME + '.txt'), 'w')
+    sys.stdout = open(join(MODELS_DIR, 'summary.txt'), 'w')
     model.summary()
     sys.stdout = old_stdout
 
-def save_performance_results(model, X_train, Y_train, X_test, Y_test):
-    out_dir = "models/"
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
 
+def save_keras_performance_results(model, model_params, X_train, Y_train, X_test, Y_test):
     ### Output training and testing scores
-    with open(os.path.join(out_dir, 'performance_' + settings.EXP_NAME + '.txt'), 'w') as fd:
+    settings.touch_dir(PERF_DIR)
+    with open(join(PERF_DIR, 'losses.txt'), 'w') as fd:
         # evaluate the model
+        fd.write("Loss function = %s\n" % model_params.loss_function)
         scores = model.evaluate(X_train, Y_train, batch_size=settings.BATCH_SIZE)
-        fd.write("Training score %s: %.4f\n" % (model.metrics_names[1], scores[1]))
+        fd.write("Training loss = %s: %.4f\n" % (model.metrics_names[1], scores[1]))
         scores = model.evaluate(X_test, Y_test, batch_size=settings.BATCH_SIZE)
-        fd.write("Testing score %s: %.4f\n" % (model.metrics_names[1], scores[1]))
-        
+        fd.write("Testing loss  = %s: %.4f\n" % (model.metrics_names[1], scores[1]))
+
+
 ## Your predictions will be saved in: predictions/assets/<experiment_name>/Y_pred_<i>.jpg
 ##                                    predictions/assets/<experiment_name>/Y_<i>.jpg
 ##                                    predictions/assets/<experiment_name>/X_outer_<i>.jpg
 ##                                    predictions/assets/<experiment_name>/X_full_<i>.jpg
 ##                                    predictions/assets/<experiment_name>/X_full_pred_<i>.jpg
-def save_predictions_info(pred, pred_indices, dataset,
-                          num_images = 20, use_flattened_datasets = True):
+def save_keras_predictions(pred, pred_indices, dataset,
+                           num_images = 20, use_flattened_datasets = True):
     if use_flattened_datasets:
-        out_dir = os.path.join('predictions/', "assets/", settings.EXP_NAME)
-        if not os.path.exists(out_dir):
-            print("Creating new directory to save predictions results: " + out_dir)
-            os.makedirs(out_dir)
-        else:
-            print("Overwriting previously saved prediction results in directory: " + out_dir)
-            
+        settings.touch_dir(ASSETS_DIR)
         for row in range(num_images):
             idt = pred_indices[row]
-            Image.fromarray(dataset.images_outer2d[idt]).save(os.path.join(out_dir, 'images_outer2d_' + str(row) + '.jpg'))
-            Image.fromarray(pred[row]).save(os.path.join(out_dir, 'images_pred_' + str(row) + '.jpg'))
-            Image.fromarray(dataset.images_inner2d[idt]).save(os.path.join(out_dir, 'images_inner2d_' + str(row) + '.jpg'))
-            Image.fromarray(dataset.images[idt]).save(os.path.join(out_dir, 'fullimages_' + str(row) + '.jpg'))
+            Image.fromarray(dataset.images_outer2d[idt]).save(join(ASSETS_DIR, 'images_outer2d_' + str(row) + '.jpg'))
+            Image.fromarray(pred[row]).save(join(ASSETS_DIR, 'images_pred_' + str(row) + '.jpg'))
+            Image.fromarray(dataset.images_inner2d[idt]).save(join(ASSETS_DIR, 'images_inner2d_' + str(row) + '.jpg'))
+            Image.fromarray(dataset.images[idt]).save(join(ASSETS_DIR, 'fullimages_' + str(row) + '.jpg'))
             fullimg_pred = np.copy(dataset.images[idt])
             center = (int(np.floor(fullimg_pred.shape[0] / 2.)), int(np.floor(fullimg_pred.shape[1] / 2.)))
             fullimg_pred[center[0]-16:center[0]+16, center[1]-16:center[1]+16, :] = pred[row, :, :, :]
-            Image.fromarray(fullimg_pred).save(os.path.join(out_dir, 'fullimages_pred_' + str(row) + '.jpg'))
+            Image.fromarray(fullimg_pred).save(join(ASSETS_DIR, 'fullimages_pred_' + str(row) + '.jpg'))
     else:
         raise NotImplementedError("Haven't implemented save_predictions_info for 2D images (only flattened images).")
 
 def print_results_as_html(pred, num_images=20):
-    html_dir = os.path.join("predictions/")
-    img_src = os.path.join("assets/", settings.EXP_NAME)
-    path_html = os.path.join(html_dir, "results_" + settings.EXP_NAME + ".html")
-    print("Saving results as html to: " + path_html)
-
-    with open(path_html, 'w') as fd:
+    settings.touch_dir(HTML_DIR)
+    img_src = "assets/"
+    html_file = join(HTML_DIR, "results.html")
+    print("Saving results as html to: " + html_file)
+    with open(html_file, 'w') as fd:
         fd.write("""
 <table>
   <tr>
