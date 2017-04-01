056a5ad55c8f84aa4c8819e88048d29f47a4490a
Philippe Paradis <philippe.paradis@gmail.com>
2017-03-31 20:00:12 -0400

    Add a convolutional model with a final FC layer

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras import optimizers
 from keras import losses
+from keras import backend as K
 
 import dcgan_lasagne
 
@@ -21,7 +22,7 @@ class ModelParameters:
         self.is_trained = False
 
 
-def build_mlp(model_params, input_dim, output_dim):
+def build_mlp(model_params):
     model = Sequential()
     model.add(Dense(units=1024, input_shape=(model_params.input_dim, )))
     model.add(Activation('relu'))
@@ -31,16 +32,12 @@ def build_mlp(model_params, input_dim, output_dim):
     model_params.is_trained = False
     return model
 
-def train_mlp(model, model_params, Dataset):
+def train_keras(model, model_params, Dataset):
     skip_ahead = False
-    
-    ### Normalize datasets
-    Dataset.normalize()
-    
-    X_train, X_test, Y_train, Y_test, id_train, id_test = Dataset.load_flattened()
+    X_train, X_test, Y_train, Y_test, id_train, id_test = Dataset.return_data()
 
     if settings.RELOAD_MODEL == True:
-        model_path = os.path.join('models/', settings.EXP_NAME + '.h5')
+        model_path = os.path.join(settings.MODELS_DIR, 'trained_model.h5')
         if os.path.isfile(model_path):
             print("The --load_model_from_file flag was passed and we found a matching model under '%s'." % model_path)
             print("Loading model from disk...")
@@ -64,27 +61,51 @@ def train_mlp(model, model_params, Dataset):
             # Fit the model
             print("Fitting model...")
             model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
-                      epochs=settings.NUM_EPOCHS, batch_size=settings.BATCH_SIZE, verbose=settings.VERBOSE)
+                      epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
 
             # evaluate the model
             print("Evaluating model...")
-            scores = model.evaluate(X_train, Y_train, batch_size=settings.BATCH_SIZE)
+            scores = model.evaluate(X_train, Y_train, batch_size=BATCH_SIZE)
             print("Training score %s: %.4f" % (model.metrics_names[1], scores[1]))
-            scores = model.evaluate(X_test, Y_test, batch_size=settings.BATCH_SIZE)
+            scores = model.evaluate(X_test, Y_test, batch_size=BATCH_SIZE)
             print("Testing score %s: %.4f" % (model.metrics_names[1], scores[1]))
             model_params.is_trained = True
 
             #%% Save model
             save_model_info(model)
         else:
-            model_path = os.path.join('models/', settings.EXP_NAME + '.h5')
+            model_path = os.path.join(settings.MODELS_DIR, 'trained_model.h5')
             print("Model was already trained, instead loading: " + model_path)
             model = load_model(model_path)
         
-    ### Denormalize all datasets
-    Dataset.denormalize()
-    
     return model
 
-def train_dcgan(Dataset):
-    dcgan_lasagne.train(Dataset, settings.NUM_EPOCHS)
+def build_conv_mlp(model_params):
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, 64, 64)
+    else:
+        input_shape = (64, 64, 3)
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    
+    model.add(Dense(units=model_params.output_dim))
+
+    model_params.is_trained = False
+
+
+def train_dcgan(Dataset, num_epochs):
+    return dcgan_lasagne.train(Dataset, num_epochs=num_epochs)
