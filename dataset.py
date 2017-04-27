f8234f7844281fbda81e055f337ccdd68bd6e8e3
Philippe Paradis <philippe.paradis@gmail.com>
2017-04-26 20:21:20 -0400

    Fix another small bug

--- dataset.py
+++ dataset.py
@@ -117,7 +117,7 @@ class BaseDataset(object):
             print(" * captions_ids.shape      = " + str(self.captions_ids.shape))
             print(" * captions_dict.shape     = " + str(self.captions_dict.shape))
             print(" * Number of color images loaded        = {}".format(self.images.shape[0]))
-            print(" * Number of greyscale images discarded = {}".format(num_rain_images_paths - self.images.shape[0]))
+            print(" * Number of greyscale images discarded = {}".format(num_train_images_path - self.images.shape[0]))
 
             # Save dataset as npy file so that loading can be sped up in the future
             self._save_jpgs_and_captions_npy()
