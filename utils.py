Add more instructive information when normalizing or denormalizing incorrect data.

utils.py
@@ -11,16 +11,20 @@ def normalize_data(data):
     """Transform linearly integers within [0, 255] to float32 within [-1, 1]. Data must be numpy array of type 'uint8'."""
     if data.dtype == 'float32':
         return
-    if np.amax(data) > 255 or np.amin(data) < 0:
-        print_error("Trying to normalize data that does not fit within the uint8 range of [0, 255].")
+    M = np.amax(data)
+    m = np.amin(data)
+    if M > 255 or m < 0:
+        print_error("Trying to normalize data that does not fit within the uint8 range of [0, 255], with max = {} and min = {}.".format(M, m))
     return (data.astype('float32') - 127.5) / 127.5
 
 def denormalize_data(data):
     """Transform linearly floating points within [-1, 1] to uint8 within [0, 255]. Data must be numpy array of type 'float32'."""
     if data.dtype == 'uint8':
         return
-    if np.amax(data) > 1.0 or np.amin(data) < -1.0:
-        print_error("Trying to normalize data that does not fit within the uint8 range of [0, 255].")
+    M = np.amax(data)
+    m = np.amin(data)
+    if M > 1.0 or m < -1.0:
+        print_error("Trying to normalize data that does not fit within the float32 range of [-1, 1], with max = {} and min = {}.".format(M, m))
     return (data*127.5 + 127.5).astype('uint8')
 
 def normalize_data_unit_interval(data):