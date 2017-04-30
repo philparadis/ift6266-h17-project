a22492b960b1b9016ad3968afcaed77e944b84fc
Philippe Paradis <philippe.paradis@gmail.com>
2017-04-30 14:10:43 -0400

    More testing

--- lsgan_architectures.py
+++ lsgan_architectures.py
@@ -87,20 +87,20 @@ def build_generator_architecture(input_var=None, architecture=1):
         # input: 100dim
         layer = app(InputLayer(shape=(None, 100), input_var=input_var))
         layer = app(GAN.GaussianNoiseLayer(layer, sigma=0.5))
-        layer = app(batch_norm(DenseLayer(layer, 512*4*4)))
+        layer = app(GAN.batch_norm(DenseLayer(layer, 512*4*4, linearity=a_fn), g=True))
         layer = app(ReshapeLayer(layer, ([0], 512, 4, 4)))
         ### four fractional-stride convolutions
         # Note: Apply dropouts in G. See tip #17 from "ganhacks"
-        layer = app(batch_norm(Deconv2DLayer(layer, 256, (5, 5), stride=(2, 2), output_size=8, nonlinearity=a_fn)))
+        layer = app(GAN.batch_norm(GAN.Deconv2DLayer(layer, 256, (5, 5), stride=(2, 2), output_size=8, nonlinearity=a_fn), g=None))
         layer = app(DropoutLayer(layer, p=0.5))
         layer = app(GAN.GaussianNoiseLayer(layer, sigma=0.2))
-        layer = app(batch_norm(Deconv2DLayer(layer, 128, (5, 5), stride=(2, 2), output_size=16, nonlinearity=a_fn)))
+        layer = app(GAN.batch_norm(GAN.Deconv2DLayer(layer, 128, (5, 5), stride=(2, 2), output_size=16, nonlinearity=a_fn), g=None))
         layer = app(DropoutLayer(layer, p=0.5))
         layer = GAN.GaussianNoiseLayer(layer, sigma=0.2)
-        layer = app(batch_norm(Deconv2DLayer(layer, 64, (5, 5), stride=(2, 2), output_size=32, nonlinearity=a_fn)))
+        layer = app(GAN.batch_norm(GAN.Deconv2DLayer(layer, 64, (5, 5), stride=(2, 2), output_size=32, nonlinearity=a_fn), g=None))
         layer = app(DropoutLayer(layer, p=0.5))
         layer = app(GAN.GaussianNoiseLayer(layer, sigma=0.5))
-        layer = app(GAN.weight_norm(Deconv2DLayer(layer, 3, (5, 5), stride=(2, 2), output_size=64, nonlinearity=T.tanh), train_g=True, init_stdv=0.1))
+        layer = app(GAN.weight_norm(GAN.Deconv2DLayer(layer, 3, (5, 5), stride=(2, 2), output_size=64, nonlinearity=T.tanh), train_g=True, init_stdv=0.1))
         print ("Generator output:", layer.output_shape)
         return layer, layers
     elif architecture = 111:
