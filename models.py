abeef94516524510059c1781aa5f0c5f69f63263
Philippe Paradis <philippe.paradis@gmail.com>
2017-05-04 22:16:37 -0400

    Add the Keras pre-trained VGG-16 model that I hadn't pushed yet.

--- models.py
+++ models.py
@@ -563,3 +563,62 @@ class GAN_BaseModel(BaseModel):
             np.savez(self.full_gen_path, *get_all_param_values(self.generator))
             np.savez(self.full_disc_path, *get_all_param_values(self.discriminator))
 
+def VGG_16(KerasModel):
+    def build(self, weights_path=None):
+        from matplotlib import pyplot as plt
+        import theano
+        import cv2
+        import numpy as np
+        import scipy as sp
+        from keras.models import Sequential
+        from keras.layers.core import Flatten, Dense, Dropout
+        from keras.layers.convolutional import Convolution2D, MaxPooling2D
+        from keras.layers.convolutional import ZeroPadding2D
+        from keras.optimizers import SGD
+        from sklearn.manifold import TSNE
+        from sklearn import manifold
+        from sklearn import cluster
+        from sklearn.preprocessing import StandardScaler
+
+        model = Sequential()
+        model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2,2), stride=(2,2)))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2,2), stride=(2,2)))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2,2), stride=(2,2)))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2,2), stride=(2,2)))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2,2), stride=(2,2)))
+        model.add(Flatten())
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1000, activation='softmax'))
+        if self.weights_path:
+            model.load_weights(self.weights_path)
+        return model
+
