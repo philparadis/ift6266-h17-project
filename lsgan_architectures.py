56c091afc3e7081dd61f6a7d23582f1b53d0d2ab
Philippe Paradis <philippe.paradis@gmail.com>
2017-05-04 23:24:10 -0400

    Fix bug in lsgan_architecture.py, where the argument pad='same' had been accidentally omitted.

--- lsgan_architectures.py
+++ lsgan_architectures.py
@@ -152,10 +152,10 @@ def build_generator_architecture(input_var=None, architecture=1):
         layer = ReshapeLayer(layer, ([0], 512, 4, 4))
         ### four fractional-stride convolutions
         # Note: Apply dropouts in G. See tip #17 from "ganhacks"
-        layer = batch_norm(Conv2DLayer(layer, 256, 7))
+        layer = batch_norm(Conv2DLayer(layer, 256, 3), stride=1, pad='same')
         layer = DropoutLayer(layer, p=0.5)
         layer = batch_norm(BilinearUpscaleLayer(layer, factor=2)) # output_size=8x8
-        layer = batch_norm(Conv2DLayer(layer, 192, 7, stride=1, pad='same'))
+        layer = batch_norm(Conv2DLayer(layer, 192, 5, stride=1, pad='same'))
         layer = DropoutLayer(layer, p=0.5)
         layer = batch_norm(BilinearUpscaleLayer(layer, factor=2)) # output_size=16x16
         layer = batch_norm(Conv2DLayer(layer, 128, 5, stride=1, pad='same'))
@@ -170,8 +170,8 @@ def build_generator_architecture(input_var=None, architecture=1):
         layer = batch_norm(BilinearUpscaleLayer(layer, factor=2)) # output_size=64x64
         layer = batch_norm(Conv2DLayer(layer, 64, 5, stride=1, pad='same'))
         layer = DropoutLayer(layer, p=0.5)
-        layer = batch_norm(Conv2DLayer(layer, 64, 3, stride=1, pad='same'))
-        layer = Conv2DLayer(layer, 3, 3, stride=1, pad='same', nonlinearity=T.tanh)
+        layer = batch_norm(Conv2DLayer(layer, 64, 5, stride=1, pad='same'))
+        layer = Conv2DLayer(layer, 3, 5, stride=1, pad='same', nonlinearity=T.tanh)
         print ("Generator output:", layer.output_shape)
         return layer, layers
     elif architecture == 2:
