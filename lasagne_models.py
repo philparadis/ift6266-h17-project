Attempting to fix normalizing issue with VGG data once and for all.

lasagne_models.py
@@ -181,7 +181,8 @@ class LasagneModel(BaseModel):
 
         # Save predictions and create HTML page to visualize them
         num_images = 100
-        denormalize_and_save_jpg_results(preds, X_test, y_test, normalize_data(dataset.test_images), num_images)
+        test_images_original = np.copy(normalize_data(dataset.test_images))
+        denormalize_and_save_jpg_results(preds, X_test, test_images_originial, y_test, , num_images)
         create_html_results_page(num_images)
 
     def create_samples(self, X, y, batch_size, num_samples, predict_fn):